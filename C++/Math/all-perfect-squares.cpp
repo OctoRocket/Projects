6a209d87ca76529d1f7a4e04c9048ea29e5b17f9
OctoRocket <xteamxeno@gmail.com>
2021-12-11 00:05:45 -0600

    Init commit

---
+++ C++/Math/all-perfect-squares.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x = 1;
+	while(true)
+	{
+		cout << x*x << endl;
+		x++;
+	}
+	return 0;
+}
---
+++ Python/every-utf-8.py
@@ -0,0 +1,8 @@
+x = 1
+while True:
+    try:
+	    print(chr(x))
+    except:
+        pass
+    x = x + 1
+
