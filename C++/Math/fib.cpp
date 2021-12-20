f97148bd3fc325e61ab32e1a756b171ef215a5f8
OctoRocket <xteamxeno@gmail.com>
2021-12-20 15:44:44 -0600

    added a c++ fib thing and it's dependancy

---
+++ C++/Math/fib.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "infint.h"
+using namespace std;
+
+void fib()
+{
+	InfInt a = 0;
+	InfInt b = 1;
+	InfInt c;
+	while(true)
+	{
+		cout << b << endl;
+		c = a + b;
+		a = b;
+		b = c;
+	}
+}
+
+int main()
+{
+	fib();
+	
+	return 0;
+}
+
