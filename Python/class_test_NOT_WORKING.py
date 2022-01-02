ea916d443ac43b123eee3a1e4b17b28f4dd3f04b
OctoRocket <xteamxeno@gmail.com>
2022-01-01 22:08:20 -0600

    Added a non working file :yay:

---
+++ Python/class_test_NOT_WORKING.py
@@ -0,0 +1,19 @@
+class test:
+    def __init__(self, test):
+        print(test)
+
+    def set_test(self, test):
+        self.jej = jej
+        return
+
+    def test(self):
+        return self.jej
+
+
+t = test
+
+t("Hello world")
+t.set_test(1)
+t.print_test()
+t.set_jej("Hello www")
+t.print_jej()
