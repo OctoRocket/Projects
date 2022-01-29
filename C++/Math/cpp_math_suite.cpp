013b85320090992df417f59c496af1d9e639e745
OctoRocket <xteamxeno@gmail.com>
2022-01-29 11:46:49 -0600

    added fib seq to the suite

--- C++/Math/cpp_math_suite.cpp
+++ C++/Math/cpp_math_suite.cpp
@@ -3,15 +3,14 @@ using namespace std;
 
 int three_x_plus_one(int num)
 {
+    cout << "Enter a number to see how long it takes to reach 1.\n";
     int count = 0;
     while (num > 1)
     {
-        if (num % 2 == 0)
-        {
+        if (num % 2 == 0) {
             num = num / 2;
         }
-        else
-        {
+        else {
             num = 3 * num + 1;
         }
         count++;
@@ -19,19 +18,41 @@ int three_x_plus_one(int num)
     return count;
 }
 
+void fib(int limit) {
+    cout << "Enter how many numbers of the fibonacci sequence you want to print out.\n";
+    int a = 0;
+    int b = 1;
+    int c;
+    for (int i = 0; i < limit; i++) {
+        cout << b << "\n";
+        c = a + b;
+        a = b;
+        b = c;
+    }
+}
+
 int main()
 {
     bool use = true;
     while (use) {
         int inp;
-        cout << "1 is a 3x+1 interface.\n";
+        cout << "1 is for 3x+1, 2 is for fib.\n";
         cin >> inp;
-        if (inp == 1)
-        {
+        if (inp == 1) {
             int num;
             cin >> num;
             cout << three_x_plus_one(num) << "\n";
         }
-        cout << "Do you want to use this program again?\n";
+        else if (inp == 2) {
+            int num;
+            cin >> num;
+            fib(num);
+        }
+        string use_deter;
+        cout << "Do you want to use this program again? (y/n)\n";
+        cin >> use_deter;
+        if (use_deter != "y") {
+            break;
+        }
     }
 }
