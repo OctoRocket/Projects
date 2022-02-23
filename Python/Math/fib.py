fb63367458abbb1e43222502fa4fdb72f4236bda
OctoRocket <xteamxeno@gmail.com>
2022-02-23 14:45:12 -0600

    Removed python and befunge

--- Python/Math/fib.py
+++
@@ -1,9 +0,0 @@
-def fib():
-    a = 0
-    b = 1
-    while True:
-        print(b)
-        c = a + b
-        a = b
-        b = c
-fib()
--- Python/every_utf_8.py
+++
@@ -1,8 +0,0 @@
-x = 1
-for i in range(150000):
-    try:
-        print(chr(x))
-    except:
-        pass
-    x = x + 1
-
