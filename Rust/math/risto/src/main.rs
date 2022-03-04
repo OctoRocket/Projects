8a66db2e8736354cdc43d2690c31a1ab911ebb1a
OctoRocket <xteamxeno@gmail.com>
2022-03-03 21:54:56 -0600

    added comments (finally)

--- Rust/math/risto/src/main.rs
+++ Rust/math/risto/src/main.rs
@@ -1,6 +1,7 @@
 use std::io;
 
 fn main() {
+    // Prepare variables
     let mut num = String::new();
     io::stdin()
         .read_line(&mut num)
@@ -9,14 +10,18 @@ fn main() {
     let mut iter = 0;
     let mut neg = false;
     loop {
+        // Target is to reach 0
         if num == 0 {
             break;
-        } else if num < 0 {
+        } else if num < 0 { // We don't want srings like 9- instead of -9 so we need to remove the negitive
             neg = true;
             num *= -1;
         }
+        // convert the int into a string then reverse it
         let temp = num.to_string().chars().rev().collect::<String>();
+        // parse string into an int again
         let mut rev_num = temp.parse::<i32>().expect("Failed to parse");
+        // if it was negitive, return the negitive symbols
         if neg == true {
             rev_num *= -1;
             num *= -1;
@@ -26,5 +31,6 @@ fn main() {
         iter += 1;
         neg = false;
     }
+    // print out how many iterations it took to get to 0
     println!("\n{}", iter);
 }
