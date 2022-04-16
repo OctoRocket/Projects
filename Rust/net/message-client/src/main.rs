09542b27d267429863d17ed61c607f57d7c8ce44
OctoRocket <xteamxeno@gmail.com>
2022-04-16 17:10:35 -0500

    Update tcp client (not working because I am lazy)

--- Rust/net/message-client/src/main.rs
+++ Rust/net/message-client/src/main.rs
@@ -1,7 +1,8 @@
-use std::{net::{TcpListener}, io::{Read, Write, stdin}};
+use std::{net::{TcpListener, TcpStream}, io::{Read, Write, stdin}, thread::spawn};
 use futures::{executor::block_on};
 
 async fn listen_print(listener: TcpListener) {
+    spawn(move || {
     for stream in listener.incoming() {
         let mut stream = stream.unwrap();
         let mut buffer = [0; 1024];
@@ -9,7 +10,7 @@ async fn listen_print(listener: TcpListener) {
         for i in buffer {
             print!("{}", i as char);
         }
-    }
+    }});
 }
 
 fn main() {
@@ -18,14 +19,14 @@ fn main() {
     stdin().read_line(&mut addr).unwrap();
     let listener = TcpListener::bind(addr.trim()).unwrap();
     block_on(listen_print(listener.try_clone().unwrap()));
-    for stream in listener.incoming() {
-        let mut stream = stream.unwrap();
-        let mut message = String::new();
-        stdin().read_line(&mut message).unwrap();
-        let mut buffer = [0; 1024];
-        for i in message.as_bytes() {
-            buffer[*i as usize] = *i;
-        }
-        stream.write(&mut buffer).unwrap();
-    }
+    
+    spawn(move || {
+        let mut stream = TcpStream::connect(addr.trim()).unwrap();
+        loop {
+            let mut buffer: Vec<u8> = Vec::new();
+            let mut message = String::new();
+            stdin().read_line(&mut message).unwrap();
+            buffer = message.as_bytes().to_vec();
+            stream.write(&buffer).unwrap();
+        }});
 }
