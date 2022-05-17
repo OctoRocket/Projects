3c3e3685eb8279bb85b9f7a3a4db816eaef0bf7d
OctoRocket <xteamxeno@gmail.com>
2022-05-17 18:52:42 -0500

    Deleted webtree because I need to make a better one

--- Python/paperclips/paperclips.py
+++
@@ -1,5 +0,0 @@
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-
-driver = webdriver.Chrome("./chromedriver")
-driver.get("https://www.decisionproblem.com/paperclips/index2.html")
