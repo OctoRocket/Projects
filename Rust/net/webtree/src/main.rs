622d81a39f1160ffbbcff13241ce996f1c46b24e
OctoRocket <xteamxeno@gmail.com>
2022-05-22 13:38:42 -0500

    Fixed and annoying bug as well as made much more progress

--- Rust/net/webtree/src/main.rs
+++ Rust/net/webtree/src/main.rs
@@ -1,8 +1,14 @@
-use reqwest::{blocking::get};
+use reqwest::blocking::get;
+
+#[derive(Debug, Clone)]
+struct Tree {
+    link: String,
+    children: Option<Vec<Tree>>,
+}
 
 fn links_on_website(website: String) -> Vec<String> {
     let mut links = Vec::new();
-    let body = get(website).unwrap().text().unwrap();
+    let body = get(&website).unwrap().text().unwrap();
     // get all links
     for link in body.match_indices("<a href=\"") {
         // get the link
@@ -10,24 +16,51 @@ fn links_on_website(website: String) -> Vec<String> {
         // add the link to the list
         links.push(link.to_string());
     }
-    // replace first / with https://
-    let mut final_links = Vec::new();
-    for link in 0..links.len() {
-        if links[link].chars().nth(0).unwrap() == '/' {
-            final_links.push("https://".to_owned() + &links[link][1..links[link].len()]);
-        } else if links[link].chars().nth(0).unwrap() != '#' {
-            final_links.push(links[link].to_owned());
+    // replace first / with website/link
+    let links = links.iter_mut().map(|link| {
+        if link.starts_with('/') {
+            *link = website.clone().trim().to_owned()+link;
+            link.to_string()
+        } else {
+            link.to_string()
         }
-    }
-    final_links
+    }).collect();
+    links
 }
 
-fn main() {
+fn init() -> Tree {
     // take user input for the url and get source of page
     let mut website = String::new();
     std::io::stdin().read_line(&mut website).unwrap();
-    let links = links_on_website(website);
-    for i in links {
-        println!("{}", i);
+    let links = links_on_website(website.clone());
+    // create a tree
+    let mut webtree = Tree {
+        link: website.trim().to_string(),
+        children: None,
+    };
+    // add all links to the tree
+    let vec_of_trees: Vec<Tree> = links.iter().map(|s| Tree { link: s.to_owned(), children: None }).collect();
+    webtree.children = Some(vec_of_trees);
+    webtree
+}
+
+fn main() {
+    let webtree = init();
+    loop {
+        // get all already found links
+        let links = format!("{:?}", webtree);
+        for i in links.match_indices("link: ") {
+            println!("{}", links[i.0..]
+                .split("\", children: ")
+                .map(|s| s
+                    .trim_start_matches("link: \"")
+                ).collect::<Vec<&str>>()[0]);
+        }
+        // get links of webtree children
+        // let mut vec_of_links = Vec::new();
+        // for i in webtree.children.iter().flatten() {
+        //     vec_of_links.push(links_on_website(i.link.clone()));
+        // }
+        break;
     }
 }
