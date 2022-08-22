bcc94d829c0bcbbc35c22f842158d9d2169242f7
OctoRocket <xteamxeno@gmail.com>
2022-08-22 15:03:25 -0500

    added thing

---
+++ Python/ss14-auto-join.py
@@ -0,0 +1,18 @@
+import requests
+import time
+import webbrowser
+
+api_url = input()
+print('\a')
+if (api_url == ""):
+    api_url = "https://lizard.spacestation14.io/server/status"
+response = requests.get(api_url)
+print(response.status_code)
+json = response.json()
+
+while (json["players"] >= 65):
+    time.sleep(15)
+    response = requests.get(api_url)
+    json = response.json()
+
+webbrowser.open('https://www.google.com/search?client=firefox-b-1-d&q=join+the+server+now')
