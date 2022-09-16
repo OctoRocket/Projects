1775f1bf6ba011e38835a48b5aa1185a8afee3a1
OctoRocket <xteamxeno@gmail.com>
2022-09-16 15:40:27 -0500

    website testing!

---
+++ Rust/net/rocket-test/src/main.rs
@@ -0,0 +1,21 @@
+#[macro_use] extern crate rocket;
+use std::path::{Path, PathBuf};
+use rocket::{fs::NamedFile, response::Redirect};
+
+
+#[get("/")]
+fn index() -> Redirect {
+    Redirect::to(uri!("/index.html"))
+}
+
+#[get("/<file..>")]
+async fn files(file: PathBuf) -> Option<NamedFile> {
+    NamedFile::open(Path::new("static/").join(file)).await.ok()
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![index])
+        .mount("/", routes![files])
+}
