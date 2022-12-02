5c9f842691243f490d081cb181e4fbeaa8e7cdfc
OctoRocket <xteamxeno@gmail.com>
2022-12-02 16:48:21 -0600

    added a radical simplifier for higher powers

---
+++ rust/math/useful/variable-radical-simplifier/src/main.rs
@@ -0,0 +1,34 @@
+use std::io::{stdin, stdout, Write};
+
+fn perfects(limit: u32, degree: u32) -> Vec<u32> {
+    let mut perfects = Vec::new();
+    let mut x: u32 = 1;
+    while x.pow(degree) <= limit {
+        perfects.push(x.pow(degree));
+        x += 1;
+    }
+    perfects
+}
+
+fn main() {
+    println!("Input radical and degree in format (inside of radical):(degree)");
+    print!("> ");
+    stdout().flush().unwrap();
+    let input: Vec<u32> = {
+        let mut inp = String::new();
+        stdin().read_line(&mut inp).unwrap();
+        inp.trim().split(":").map(|f| f.parse().unwrap()).collect()
+    };
+    let mut perfects = perfects(input[0], input[1]);
+    perfects.reverse();
+    for i in perfects {
+        if input[0] % i == 0 {
+            if input[0] / i == 1 {
+                println!("{}", i / input[1]);
+                return;
+            }
+            println!("{}rad({})", i / input[1], input[0] / i);
+            return;
+        }
+    }
+}
