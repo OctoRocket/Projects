ded3495ac7b76edd1481378eeacf415471af7cf7
OctoRocket <xteamxeno@gmail.com>
2023-01-18 15:34:07 -0600

    refactored risto

--- rust/math/fun/risto/src/main.rs
+++ rust/math/fun/risto/src/main.rs
@@ -1,41 +1,35 @@
-use std::io;
+use std::io::stdin;
 
-fn main() {
-    // Prepare variables
-    let mut num = String::new();
-    io::stdin()
-        .read_line(&mut num)
-        .expect("Failed to read line");
-    let mut num = num.trim().parse::<i32>().expect("Failed to parse");
-    let mut iter = 0;
-    let mut vec:Vec<i32> = Vec::new();
-    loop {
-        // Target is to reach 0
-        if num == 0 {
-            break;
-        }
-        // convert the int into a string then reverse it
-        let mut temp = num.to_string().chars().rev().collect::<String>();
-        // fix negitive issues
-        if num < 0 {
-            temp.pop();
-            temp.insert(0, '-');
-        }
-        // parse string into an int again
-        let rev_num = temp.parse::<i32>().expect("Failed to parse");
-        // if it was negitive, return the negitive symbols
-        println!("num: {}, rev_num: {}", num, rev_num);
-        num -= rev_num;
-        iter += 1;
-        // detect if there is a loop
-        for i in &mut vec {
-            if num == *i {
-                println!("Loop detected at iteration {}! Number: {}", iter, num);
-                return;
-            }
-        }
-        vec.push(num);
+// This is a function to flip a number
+fn flip(input: i32) -> i32 {
+    if input.is_negative() {
+        return -input.abs().to_string().chars().rev().collect::<String>().parse::<i32>().unwrap();
     }
-    // print out how many iterations it took to get to 0
-    println!("\n{}", iter);
+    input.to_string().chars().rev().collect::<String>().parse::<i32>().unwrap()
+}
+
+// This fuction contains the logic for the program
+fn risto(mut input: i32) -> Vec<i32> {
+    let mut number_list = Vec::new();
+    while input != 0 || !number_list.contains(&input) {
+        number_list.push(input);
+        input -= flip(input);
+    }
+    number_list.pop();
+    number_list
+}
+
+/* All that main does is handle the input from stdin
+   and making the output of risto look nicer         */
+fn main() {
+    let input = {
+        let mut buf = String::new();
+        stdin().read_line(&mut buf).unwrap();
+        buf.trim().parse().expect("Parse error")
+    };
+    let output = risto(input);
+    println!("It took {} iterations to get to 0,", output.len());
+    for i in output {
+        println!("{} - {} = {}", i, flip(i), i - flip(i));
+    }
 }
