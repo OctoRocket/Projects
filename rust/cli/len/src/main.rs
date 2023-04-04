6fd212791b39506385c007b123bf7330356825c3
OctoRocket <xteamxeno@gmail.com>
2023-04-04 18:04:58 -0500

    no allocation (I think)

--- rust/cli/len/src/main.rs
+++ rust/cli/len/src/main.rs
@@ -1,6 +1,5 @@
 use std::env::args;
 
 fn main() {
-    let arg = &args().collect::<Vec<String>>()[1];
-    println!("{}", arg.len());
+    println!("{}", args().collect::<Vec<String>>()[1].len());
 }
