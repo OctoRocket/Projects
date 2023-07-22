357ec1c4049b8beae539e0b79cd4ee7bd2ce69c1
OctoRocket <xteamxeno@gmail.com>
2023-07-22 18:25:36 -0500

    fraction simplifier clippy changes

--- rust/math/useful/fraction-simplifier/src/main.rs
+++ rust/math/useful/fraction-simplifier/src/main.rs
@@ -13,26 +13,26 @@ fn gcd(mut n1: u32, mut n2: u32) -> u32 {
             } else {
                 return n2;
             }
-        } else if n1 > n2 || n1 == n2 {
-            n1 = n1 % n2;
+        } else if n1 >= n2 {
+            n1 %= n2;
         } else if n2 > n1 {
-            n2 = n2 % n1;
+            n2 %= n1;
         }
     };
 }
 
 fn simplify(input: String) -> String {
-    let nums: Vec<u32> = input.split("/").map(|f| f.parse::<u32>().unwrap()).collect();
+    let nums: Vec<u32> = input.split('/').map(|f| f.parse::<u32>().unwrap()).collect();
     let gcd = gcd(nums[0], nums[1]);
     format!("{}/{}", nums[0]/gcd, nums[1]/gcd)
 }
 
 fn main() {
     let input = input();
-    if {
-        let check: Vec<&str> = input.split("/").collect();
+    if !{
+        let check: Vec<&str> = input.split('/').collect();
         check[0].parse::<u32>().is_ok() && check[1].parse::<u32>().is_ok()
-    } == false {
+    } {
         println!("Not a fraction");
         return;
     }
