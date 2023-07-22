7271620c7c1522db2472738caded436e89fd07bc
OctoRocket <xteamxeno@gmail.com>
2023-07-22 18:03:31 -0500

    improve factors

--- rust/math/useful/factors/src/main.rs
+++ rust/math/useful/factors/src/main.rs
@@ -1,20 +1,29 @@
+use std::env::args;
+
 fn main() {
-    let n = match std::env::args().collect::<Vec<String>>()[1].parse() {
-        Ok(x) => x,
-        Err(e) => {
-            println!("Failed to parse, is your input a positve number? ({})", e);
-            return;
-        }
-    };
+    let input = args()
+        .nth(1)
+        .expect("Please provide an input")
+        .trim()
+        .parse()
+        .expect("Please provide a number");
+
+    // verify that the input is positive
+    if input < 1 {
+        panic!("Please provide a positive number");
+    }
+
     // get a range of numbers from 1 to n, then filter it so that only factors remain
-    let range = (1..n).filter(|x| n % x == 0);
-    let mut vec = vec!["\n".to_string()];
+    let range = (1..input).filter(|x| input % x == 0);
+    let mut vec = Vec::new();
+
     // format it correctly
     for i in range {
-        if n % i == 0 {
-            vec.push((n/i).to_string() + "*" + &i.to_string());
+        if input % i == 0 {
+            vec.push((input/i).to_string() + " * " + &i.to_string());
         }
     }
+
     // remove half of the list, as half of it will just be the flipped version of the other half (eg. 9*2 and 2*9)
     let flip = usize::from(vec.len() % 2 == 1);
     if vec.len() != 2 {
@@ -22,8 +31,9 @@ fn main() {
             vec.pop();
         }
     }
-    // print
+
+    // print the result
     for i in vec {
-        println!("{}", i);
+        println!("{i}");
     }
 }
