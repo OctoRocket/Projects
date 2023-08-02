7c63a2e049ddbc51eccb2f5954043bf79c05deef
OctoRocket <xteamxeno@gmail.com>
2023-08-01 21:46:23 -0500

    Increase readability

--- rust/misc/ftl-accent-cli-interface/src/main.rs
+++ rust/misc/ftl-accent-cli-interface/src/main.rs
@@ -1,65 +1,69 @@
+use anyhow::{
+    Result,
+    anyhow,
+};
 use std::{
     io::stdin,
     fs::read_to_string,
     collections::HashMap,
 };
 
-#[derive(Debug)]
-struct FtlToJsonError {
-    // This code isn't actually dead, it is used when the program errors.
-    #[allow(dead_code)]
-    error: String,
-}
-
-impl From<std::io::Error> for FtlToJsonError {
-    fn from(e: std::io::Error) -> Self {
-        FtlToJsonError{ error: e.to_string() }
-    }
-}
-
-type FtlToJsonResult = Result<(HashMap<String, String>, HashMap<String, String>), FtlToJsonError>;
-
-fn take_input() -> Result<String, FtlToJsonError> {
+fn take_input() -> Result<String> {
     let mut buf = String::new();
     stdin().read_line(&mut buf)?;
     Ok(buf)
 }
 
-fn file_to_hashes(file: String) -> FtlToJsonResult {
-    let binding = read_to_string(file)?;
-    let mut to_hash: HashMap<String, String> = HashMap::new();
-    let mut from_hash: HashMap<String, String> = HashMap::new();
-    let ftl = binding
-        .split('\n')
-        .filter(|line| line != &"")
-        .map(|line| line.split(" = ").collect::<Vec<&str>>())
-        .collect::<Vec<Vec<&str>>>();
-    for line in ftl {
-        let stringified_line = (line[0].to_string(), line[1].to_string());
-        if line[0].contains("replaced") {
-            to_hash.insert(stringified_line.1, stringified_line.0);
-        } else if line[0].contains("replacement") {
-            from_hash.insert(stringified_line.0.replace("replacement", "replaced"), stringified_line.1);
-        } else {
-            return Err(FtlToJsonError{ error: "File formatted incorrectly.".to_string() });
-        }
-    }
-    Ok((to_hash, from_hash))
+fn parse(file: String) -> Result<Vec<Vec<String>>> {
+    let file = read_to_string(file)?; // Read file
+    let ftl = file
+        .split('\n') // Split by new line
+        .filter(|line| line != &"") // Remove empty lines
+        .map(|line| line.split(" = ") // Split into the fore and the aft section
+            .map(|word| word.to_string()) // Convert to string
+            .collect::<Vec<String>>())
+        .collect::<Vec<Vec<String>>>();
+    Ok(ftl)
 }
 
-fn main() -> Result<(), FtlToJsonError> {
+fn file_to_hashes(file: String) -> Result<(HashMap<String, String>, HashMap<String, String>)> {
+    let ftl = parse(file)?;
+    let mut replaced_words: HashMap<String, String> = HashMap::new();
+    let mut replacement_words: HashMap<String, String> = HashMap::new();
+
+    for line in ftl {
+        let pair = (&line[0], &line[1]);
+        if line[0].contains("replaced") { // If the word is a replaced word
+            replaced_words.insert(pair.1.clone(), pair.0.clone());
+        } else if line[0].contains("replacement") { // If the word is a replacement word
+            replacement_words.insert(pair.0.replace("replacement", "replaced"), pair.1.clone());
+        } else {
+            return Err(anyhow!("Invalid FTL file"));
+        }
+    }
+    Ok((replaced_words, replacement_words))
+}
+
+fn main() -> Result<()> {
+    // Get file input
     println!("Path to FTL file (with comments removed)");
     let file_input = match take_input()?.trim().to_string() {
-        s if s == "" => "archaic.ftl".to_string(),
+        s if s.is_empty() => "archaic.ftl".to_string(),
         s => s,
     };
-    let (to_hash, from_hash) = file_to_hashes(file_input)?;
+    
+    // Get the hashes
+    let (replaced_words, replacement_words) = file_to_hashes(file_input)?;
+
+    // Get user input
     println!("Input a sentance");
     loop {
         let mut input = take_input()?.to_lowercase().trim().to_string();
+
+        // Replace words
         for word in input.clone().split(' ') {
-            if to_hash.contains_key(word) {
-                input = input.replace(word, from_hash[&to_hash[word]].as_str());
+            if replaced_words.contains_key(word) {
+                input = input.replace(word, replacement_words[&replaced_words[word]].as_str());
             }
         }
         println!("{input}");
