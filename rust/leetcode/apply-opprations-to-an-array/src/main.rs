039a0b31cea6313e6dc0ddd58e318f3119a3f5d7
OctoRocket <xteamxeno@gmail.com>
2023-09-21 19:18:12 -0500

    can we leet much further

--- rust/leetcode/apply-opprations-to-an-array/src/main.rs
+++ rust/leetcode/apply-opprations-to-an-array/src/main.rs
@@ -8,22 +8,21 @@ struct Solution;
 
 impl Solution {
     #[allow(clippy::needless_pass_by_value)]
-    pub fn apply_operations(nums: Vec<i32>) -> Vec<i32> {
+    pub fn apply_operations(mut nums: Vec<i32>) -> Vec<i32> {
         let mut zero_counter = nums.iter().filter(|n| n == &&0).count();
-        let mut new_nums = nums;
 
-        for i in 1..new_nums.len() {
-            if new_nums[i - 1] == new_nums[i] && new_nums[i] != 0 {
-                new_nums[i - 1] *= 2;
-                new_nums[i] = 0;
+        for i in 1..nums.len() {
+            if nums[i - 1] == nums[i] && nums[i] != 0 {
+                nums[i - 1] *= 2;
+                nums[i] = 0;
                 zero_counter += 1;
             }
         }
         
-        new_nums = new_nums.iter().map(::std::borrow::ToOwned::to_owned).filter(|n| n != &0).collect::<Vec<i32>>();
-        new_nums.resize(new_nums.len() + zero_counter, 0);
+        nums = nums.iter().map(|&n| n).filter(|&n| n != 0).collect::<Vec<i32>>();
+        nums.resize(nums.len() + zero_counter, 0);
 
-        new_nums
+        nums
     }
 }
 
