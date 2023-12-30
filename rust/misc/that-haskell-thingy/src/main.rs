8b374912c0e7f85475c62dd3d034eaf85aab6b18
OctoRocket <xteamxeno@gmail.com>
2023-12-30 14:34:19 -0600

    more comparison

--- rust/misc/that-haskell-thingy/src/main.rs
+++ rust/misc/that-haskell-thingy/src/main.rs
@@ -1,5 +1,5 @@
 fn elem<T>(val: T, list: Vec<T>) -> bool
-where T: Eq, T: std::clone::Clone {
+where T: Eq, T: std::clone::Clone, {
     if list.is_empty() {
         return false;
     }
@@ -14,6 +14,12 @@ where T: Eq, T: std::clone::Clone {
     (first == &val) || (elem(val, rest))
 }
 
+fn elem_2<T>(val: T, list: Vec<T>) -> bool
+where T: Eq, {
+    list.iter().filter(|x| x == &&val).count() >= 1
+}
+
 fn main() {
     println!("{}", elem(3, (1..=10).collect()));
+    println!("{}", elem_2(3, (1..=10).collect()));
 }
