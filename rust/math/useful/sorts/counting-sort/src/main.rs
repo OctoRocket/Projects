e0035f3e0b2d56c4fb6b55d3115c13d0218e3f38
OctoRocket <88291550+OctoRocket@users.noreply.github.com>
2024-05-04 22:23:10 +0000

    generics for counting sort

--- rust/math/useful/sorts/counting-sort/src/main.rs
+++ rust/math/useful/sorts/counting-sort/src/main.rs
@@ -1,6 +1,14 @@
-use std::convert::TryInto;
+use std::convert::{TryFrom, TryInto};
+use std::fmt::Debug;
+use std::ops::{Add, Sub};
 
-fn counting_sort(list: &mut Vec<i32>) {
+fn counting_sort<T>(list: &mut Vec<T>)
+where
+    T: Ord + Copy + TryFrom<usize> + Add<Output = T> + Sub<Output = T>,
+    <T as TryFrom<usize>>::Error: Debug,
+    usize: TryFrom<T>,
+    <usize as TryFrom<T>>::Error: Debug,
+{
     if list.is_empty() {
         return;
     }
@@ -8,7 +16,7 @@ fn counting_sort(list: &mut Vec<i32>) {
     let max = *list.iter().max().unwrap();
     let min = *list.iter().min().unwrap();
 
-    let mut counts: Vec<usize> = vec![0; <i32 as TryInto<usize>>::try_into(max - min).unwrap() + 1];
+    let mut counts: Vec<usize> = vec![0; <T as TryInto<usize>>::try_into(max - min).unwrap() + 1];
 
     for number in &mut *list {
         let index: usize = (*number - min).try_into().unwrap();
@@ -18,10 +26,10 @@ fn counting_sort(list: &mut Vec<i32>) {
     let mut built = vec![];
     for (index, _) in counts.iter().enumerate() {
         for _ in 0..counts[index] {
-            built.push(<usize as TryInto<i32>>::try_into(index).unwrap() + min);
+            built.push(<usize as TryInto<T>>::try_into(index).unwrap() + min);
         }
     }
-    
+
     *list = built;
 }
 
