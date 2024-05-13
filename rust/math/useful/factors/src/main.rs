02ea0e73f62b88ab83c5899680b2e3ad59c953f2
OctoRocket <xteamxeno@gmail.com>
2024-05-13 23:07:29 +0000

    update factors

--- rust/math/useful/factors/src/main.rs
+++ rust/math/useful/factors/src/main.rs
@@ -1,5 +1,19 @@
 use std::env::args;
 
+const fn sqrt(num: usize) -> usize {
+    let mut root = 1;
+
+    while (root + 1) * (root + 1) <= num {
+        root += 1;
+    }
+
+    root
+}
+
+fn get_factors(num: usize) -> impl Iterator<Item = (usize, usize)> {
+    (1..=sqrt(num)).map(move |n| (n, num / n))
+}
+
 fn main() {
     let input = args()
         .nth(1)
@@ -8,32 +22,5 @@ fn main() {
         .parse()
         .expect("Please provide a number");
 
-    // verify that the input is positive
-    if input < 1 {
-        panic!("Please provide a positive number");
-    }
-
-    // get a range of numbers from 1 to n, then filter it so that only factors remain
-    let range = (1..input).filter(|x| input % x == 0);
-    let mut vec = Vec::new();
-
-    // format it correctly
-    for i in range {
-        if input % i == 0 {
-            vec.push((input / i).to_string() + " * " + &i.to_string());
-        }
-    }
-
-    // remove half of the list, as half of it will just be the flipped version of the other half (eg. 9*2 and 2*9)
-    let flip = usize::from(vec.len() % 2 == 1);
-    if vec.len() != 2 {
-        for _i in vec.len() / 2 + 1..vec.len() - flip {
-            vec.pop();
-        }
-    }
-
-    // print the result
-    for i in vec {
-        println!("{i}");
-    }
+    get_factors(input).for_each(|p| println!("{} x {}", p.0, p.1));
 }
