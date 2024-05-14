a4c3955f6c560d969162c618c4c507525adc3a75
OctoRocket <xteamxeno@gmail.com>
2024-05-14 01:15:37 +0000

    Factor in Clojure

---
+++ clojure/factors/src/factors/main.clj
@@ -0,0 +1,27 @@
+(ns factors.main)
+
+(defn sift "Keep elements of one list based on the elements of another."
+  [v f]
+  (map first (filter (fn [[_ bool]] bool) (map vector v f))))
+
+(defn int-sqrt "Return the integer closest to the square root of a number 
+                without going over."
+  ([n] (int-sqrt n n))
+  ([n m] (if (<= (* n n) m) n (int-sqrt (- n 1) m))))
+
+(defn factor "Return all the factors of a number."
+  [n]
+  (let
+   [u (range 1 (+ 1 (int-sqrt n)))]
+   (map #(vector % (/ n %)) (sift u (map #(== 0 (mod n %)) u)))))
+
+(defn -main [] (doseq [
+                       l
+                       (map 
+                        #(str (first %) " x " (last %)) 
+                        (factor 16))] 
+                 (println l)))
+
+(factor 16)
+
+;; (-main)
