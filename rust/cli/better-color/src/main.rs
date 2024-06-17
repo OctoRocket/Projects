734c0f71538f3668507afeb1e9f8510aadd63b7c
OctoRocket <xteamxeno@gmail.com>
2024-06-16 21:55:11 -0500

    prepare for revamp

--- rust/cli/better-color/src/main.rs
+++ rust/cli/better-color/src/main.rs
@@ -1,48 +1,78 @@
 #![deny(clippy::all)]
 #![warn(
     clippy::pedantic,
-    clippy::nursery,
+    clippy::nursery
 )]
 
+use anyhow::Result;
+use colored::{ Color, Colorize };
+use regex::Regex;
 use std::{
-    fs::File,
-    path::PathBuf,
-    io::Read,
     env::args,
+    fs::File,
+    io::Read,
+    path::PathBuf,
     thread::sleep,
     time::Duration,
 };
-use anyhow::{
-    Result,
-    Error,
-};
-use colored::{
-    Color,
-    Colorize,
-};
-use regex::Regex;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
 enum ProgramError {
-    #[error("no path provided")]
+    #[error("No path provided.")]
     Path,
 
-    #[error("missing color tag")]
+    #[error("Missing or broken color tag.")]
     ColorTag,
 
-    #[error("text malformation error")]
+    #[error("Some text is malformed")]
     Text,
 
-    #[error("unindentifiable color tag: {0}")]
+    #[error("Unindentifiable color tag: {0}")]
     Conversion(String),
 }
 
 #[derive(Debug)]
-struct Text {
+struct TextBlock {
     color_tag: Color,
-    content: String
+    content: String,
 }
+
+fn main() -> Result<()> {
+    let path = args().nth(1).ok_or(ProgramError::Path)?;
+
+    loop {
+        print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
+        let file = File::open(PathBuf::from(path.clone()))?;
+
+        match parse_file(file.try_clone()?) {
+            Ok(v) => display_text(v),
+            Err(e) => println!("{e}"),
+        };
+        println!();
+
+        sleep(Duration::from_millis(500));
+    }
+}
+
+fn parse_file(mut file: File) -> Result<Vec<TextBlock>> {
+    let re = Regex::new(r"\{[^{]*")?;
+    let mut file_buf = String::new();
+    file.read_to_string(&mut file_buf)?;
+    let prepended_file = String::from(r"{c0}") + &file_buf;
+
+    let segments = re
+        .find_iter(&prepended_file)
+        .map(|m| m.as_str().to_string());
+    let mut texts = Vec::new();
+
+    for s in segments {
+        texts.push(split_text(&s)?);
+    }
+
+    Ok(texts)
+}
+
 
 fn to_color(text: &str) -> Result<Color> {
     match text {
@@ -54,57 +84,23 @@ fn to_color(text: &str) -> Result<Color> {
         r"{c5}" => Ok(Color::Cyan),
         r"{c6}" => Ok(Color::Magenta),
         r"{c7}" => Ok(Color::Black),
-        t => Err(Error::new(ProgramError::Conversion(t.to_string()))),
+        t => Err(ProgramError::Conversion(t.to_string()).into()),
     }
 }
 
-fn split_text(text: &str) -> Result<Text> {
+fn split_text(text: &str) -> Result<TextBlock> {
     let re = Regex::new(r"\{.*?\}")?;
     let color_tag = re.find(text).ok_or(ProgramError::ColorTag)?.as_str();
     let content = re.split(text).nth(1).ok_or(ProgramError::Text)?;
 
-    Ok(Text{
+    Ok(TextBlock {
         color_tag: to_color(color_tag)?,
-        content: content.to_string()
+        content: content.to_string(),
     })
 }
 
-fn parse_file(mut file: File) -> Result<Vec<Text>> {
-    let re = Regex::new(r"\{[^{]*")?;
-    let mut file_buf = String::new();
-    file.read_to_string(&mut file_buf)?;
-    let prepended_file = String::from(r"{c0}") + &file_buf;
-
-    let segments = re.find_iter(&prepended_file).map(|m| m.as_str().to_string());
-    let mut texts = Vec::new();
-
-    for s in segments {
-        texts.push(split_text(&s)?);
-    }
-
-    Ok(texts)
-}
-
-fn display_text(text: Vec<Text>) {
+fn display_text(text: Vec<TextBlock>) {
     for segment in text {
         print!("{}", segment.content.as_str().color(segment.color_tag));
-    }
-}
-
-fn main() -> Result<()> {
-    let path = args().nth(1).ok_or(ProgramError::Path)?;
-    
-    loop {
-        print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
-        let file = File::open(PathBuf::from(path.clone()))?;
-
-        match parse_file(file.try_clone()?)
-        {
-            Ok(v) => display_text(v),
-            Err(e) => println!("{e}"),
-        };
-        println!();
-
-        sleep(Duration::from_millis(250));
     }
 }
