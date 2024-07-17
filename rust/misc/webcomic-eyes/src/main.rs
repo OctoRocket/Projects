467744b3e437d3a319ddca5e000caf3860cc03ab
OctoRocket <xteamxeno@gmail.com>
2024-07-17 00:26:22 -0500

    basic spinner functionality, more needs to be added

---
+++ rust/misc/webcomic-eyes/src/main.rs
@@ -0,0 +1,103 @@
+use indoc::indoc;
+use std::{io::{self, Stdout, Write}, process, thread::sleep, time::Duration};
+use crossterm::{cursor, style::Stylize, terminal, ExecutableCommand, QueueableCommand};
+
+// Eyes for when important characters die, flipping between the eight judgements
+// Also could be used for mysterious important characters with more of an
+// influence on the story.
+const DYING_STATES: &[&str] = &[
+    indoc! {"
+      |
+      *
+     / \\
+    "},
+    indoc! {"
+     \\
+      *--
+      |
+    "},
+    indoc! {"
+       /
+    --*
+       \\
+    "},
+    indoc! {"
+      |
+      *--
+     /
+    "},
+    indoc! {"
+    \\ /
+     *
+     |
+    "},
+    indoc! {"
+      |
+    --*
+       \\
+    "},
+    indoc! {"
+    \\
+     *--
+    /
+    "},
+    indoc! {"
+       /
+    --*
+      |
+    "},
+];
+
+fn clear(stdout_handle: &mut Stdout, previous_state_height: u16) {
+    let current_row = cursor::position().unwrap().1;
+    for row in (current_row - previous_state_height)..=current_row {
+        let width = terminal::size().unwrap().0;
+        stdout_handle.queue(cursor::MoveTo(0, row)).unwrap();
+        stdout_handle.write_all(&vec![b' '; width as usize]).unwrap();
+    }
+    stdout_handle.queue(cursor::MoveTo(0, current_row - previous_state_height)).unwrap();
+}
+
+// The unwrapping is made under the assumption that it would have errored
+// earlier if it will have errored.
+fn main() {
+    // Set the terminal title
+    let mut stdout_handle = io::stdout();
+    let _ = stdout_handle.execute(terminal::SetTitle("Webcomic concept art!"));
+
+    // Move to a blank screen
+    if let Err(e) = stdout_handle.execute(terminal::EnterAlternateScreen) {
+        eprintln!("{}", "Your terminal doesn't support alternate screens!".red());
+        eprintln!("{}", "The program might not work as intended.".red());
+        eprintln!("{} {}", "Exact error:".dark_grey(), e.to_string().dark_grey());
+    }
+
+    // Make cursor visble when animation is killed.
+    if let Err(e) = ctrlc::set_handler(|| {
+        let mut stdout_handler = io::stdout();
+        stdout_handler.execute(cursor::Show).unwrap();
+        stdout_handler.execute(terminal::LeaveAlternateScreen).unwrap();
+        process::exit(1);
+    }) {
+        eprintln!("{}", "Failed to set ctrl + c handler!".red());
+        eprintln!("Continuing would cause your cursor to be outside of this process invisble.");
+        eprintln!("{} {}", "Exact error:".dark_grey(), e.to_string().dark_grey());
+        return;
+    }
+
+    // Make cursor invisble on startup for animations.
+    if let Err(e) = stdout_handle.queue(cursor::Hide) {
+        eprintln!("{}", "Queue to stdout! Your terminal is most likely unusable!".red());
+        eprintln!("{} {}", "Exact error:".dark_grey(), e.to_string().dark_grey());
+        return;
+    }
+
+    loop {
+        for state in DYING_STATES {
+            stdout_handle.write_all(state.as_bytes()).unwrap();
+            stdout_handle.flush().unwrap();
+            sleep(Duration::from_millis(250));
+            clear(&mut stdout_handle, state.lines().count() as u16);
+        }
+    }
+}
