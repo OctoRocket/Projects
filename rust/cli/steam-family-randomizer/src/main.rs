5ea88d4c98bf04e2f77de72b2dff00c59c25e3cf
OctoRocket <xteamxeno@gmail.com>
2026-06-19 13:55:26 -0500

    whatever

--- rust/cli/steam-family-randomizer/src/main.rs
+++ rust/cli/steam-family-randomizer/src/main.rs
@@ -7,24 +7,53 @@ use args::Args;
 
 use clap::Parser;
 use anyhow::Result;
-use std::{fs::File, io::Read, path::PathBuf};
+use reqwest::Client;
+use std::{fs::File, io::{self, Read}};
+use rand::prelude::*;
 
-use crate::parser::{all_ids, steam_id, first_id};
+use crate::parser::all_ids;
 
-fn main() -> Result<()> {
+macro_rules! time {
+    ($code:expr) => {{
+        let prev = ::std::time::Instant::now();
+        let res = $code;
+        let now = ::std::time::Instant::now();
+        eprintln!("Took {:?}", now - prev);
+        res
+    }};
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
     let args = Args::parse();
+    let mut rng = rand::rng();
 
-    let mut file = File::open(PathBuf::from("example2.har")).unwrap();
+    // let http_client = Client::new();
+
+    let mut file = File::open(args.file).unwrap();
     let mut buf = String::new();
-    file.read_to_string(&mut buf);
-    let mut v = all_ids(&buf);
-    v.sort_unstable();
-    v.dedup();
-    eprintln!("{v:?}, {}", v.len());
+    eprintln!("Reading file...");
+    time!(file.read_to_string(&mut buf)?);
+    eprintln!("Extracting IDs...");
+    let mut ids = time!(all_ids(&buf));
+    ids.sort_unstable();
+    ids.dedup();
+
+    eprintln!("Fetching game names...");
+    // let all_game_info = time!(get_game_infos(http_client, ids).await?);
+
+    let input = io::stdin();
+    let mut all_game_names = ids;
+    loop {
+        let mut buf = String::new();
+        input.read_line(&mut buf)?;
+        all_game_names.shuffle(&mut rng);
+        println!("https://www.protondb.com/app/{}", all_game_names[0]);
+    }
 
     // let har_file = File::open(args.file)?;
     // let parsed_har = parse_har(har_file)?;
     // let steam_game_ids = get_steam_game_ids(parsed_har);
 
-    Ok(())
+    // Ok(())
 }
--- rust/cli/steam-family-randomizer/src/parser.rs
+++ rust/cli/steam-family-randomizer/src/parser.rs
@@ -4,7 +4,6 @@ use nom::{
     bytes::complete::{
         tag,
         take_till,
-        take_till1,
         take_until1,
     },
     sequence::{
@@ -13,8 +12,6 @@ use nom::{
         preceded,
     },
     character::complete::digit1,
-    combinator::recognize,
-    branch::alt,
     multi::many0,
     error::{Error, ErrorKind},
     Err,
@@ -38,11 +35,6 @@ pub fn steam_id(input: &str) -> IResult<&str, &str> {
     string.and_then(preceded((https, domain, take_till(|c: char| c.is_ascii_digit())), digit1)).parse(input)
 }
 
-fn url(input: &str) -> IResult<&str, &str> {
-    // url is {protocol}://{domain}{/[path]}
-    string.and_then(recognize((https, domain, path))).parse(input)
-}
-
 fn string(input: &str) -> IResult<&str, &str> {
     delimited(tag("\""), take_until1("\""), tag("\"")).parse(input)
 }
@@ -54,36 +46,4 @@ fn https(input: &str) -> IResult<&str, &str> {
 fn domain(input: &str) -> IResult<&str, &str> {
     // take_till1(|c| c == '/').parse(input)
     tag("shared.fastly.steamstatic.com").parse(input)
-}
-
-fn path(input: &str) -> IResult<&str, &str> {
-    recognize((
-        many0((tag("/"), take_till1(|c| c == '/' || c == '?' || c == '\\'))),
-        alt((tag("/"), tag(""))),
-    )).parse(input)
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    use std::{fs::File, io::Read, path::PathBuf};
-
-    #[test]
-    fn test_url() {
-        url(r#""https://asta.octorocket.dev/index.html""#).unwrap();
-    }
-
-    #[test]
-    fn test_steam_id() {
-        steam_id(r#""https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/945360/library_600x900.jpg""#).unwrap();
-    }
-
-    #[test]
-    fn test_first_url() {
-        let mut file = File::open(PathBuf::from("example.har")).unwrap();
-        let mut buf = String::new();
-        file.read_to_string(&mut buf);
-        dbg!(url(&buf).unwrap());
-    }
 }
